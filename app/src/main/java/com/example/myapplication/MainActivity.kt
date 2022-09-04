9043781d22dbfae747ed3f4c97acddba65ceae2f
Hunvil Rodrigues <hrodrigues@slack-corp.com>
2022-09-04 15:23:19 -0700

    adding check for keyboard visibility

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowInsets
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.core.view.doOnLayout
 import com.example.myapplication.main.MainScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -28,6 +34,34 @@ class MainActivity : ComponentActivity() {
                 ) {
                     MainScreen()
                 }
+            }
+        }
+        window.decorView.addKeyboardInsetListener {
+            Log.d("TAG", "hjr it $it")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun View.addKeyboardInsetListener(keyboardCallback: (visible: Boolean) -> Unit) {
+        doOnLayout {
+            //get init state of keyboard
+            var keyboardVisible = rootWindowInsets?.isVisible(WindowInsets.Type.ime()) == true
+
+            //Log.d("TAG", "hjr keyboardVisible $keyboardVisible")
+            //callback as soon as the layout is set with whether the keyboard is open or not
+            keyboardCallback(keyboardVisible)
+
+            //whenever there is an inset change on the App, check if the keyboard is visible.
+            setOnApplyWindowInsetsListener { _, windowInsets ->
+                val keyboardUpdateCheck =
+                    rootWindowInsets?.isVisible(WindowInsets.Type.ime()) == true
+                //since the observer is hit quite often, only callback when there is a change.
+                if (keyboardUpdateCheck != keyboardVisible) {
+                    keyboardCallback(keyboardUpdateCheck)
+                    keyboardVisible = keyboardUpdateCheck
+                }
+
+                windowInsets
             }
         }
     }
--- app/src/main/java/com/example/myapplication/greeting/HelloContent.kt
+++ app/src/main/java/com/example/myapplication/greeting/HelloContent.kt
@@ -2,16 +2,24 @@ package com.example.myapplication.greeting
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun HelloContent() {
+    val focusManager = LocalFocusManager.current
     Column(modifier = Modifier.padding(8.dp)) {
         Text(
             text = "hello",
@@ -19,9 +27,18 @@ fun HelloContent() {
             style = MaterialTheme.typography.h5
         )
         OutlinedTextField(
+            keyboardOptions = KeyboardOptions.Default.copy(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
             value = "",
             onValueChange = {},
-            label = { Text(text = "Name") }
+            label = { Text(text = "Name") },
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.moveFocus(FocusDirection.Down) }
+            )
         )
     }
 }
